e2a5a6ed649bdbfbcc0748562387ead30d05b26c
Daniel Schwan <daniel.schwan@it-designers.de>
2020-01-31 23:02:55 +0100

    added some ui elements

---
+++ Scripts/goback_b_script.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _on_GO_BACK_pressed():
+	get_tree().change_scene("res://Scenes/start_screen.tscn")
---
+++ Scripts/sound_slider_skript.gd
@@ -0,0 +1,4 @@
+extends HSlider
+
+func _on_sound_slider_value_changed(value):
+	print(value)
