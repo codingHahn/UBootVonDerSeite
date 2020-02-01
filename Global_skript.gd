c293517437bb701fd7d5b093925ed435cbb8f37e
Daniel Schwan <daniel.schwan@it-designers.de>
2020-02-01 19:22:36 +0100

    some fixes

--- Global_skript.gd
+++ Global_skript.gd
@@ -3,13 +3,19 @@ extends Node
 export (Array) var PlayerList
 
 func _ready():
-	PlayerList.append("player_1")
+	_resetPlayerList()
 
 func _player2_ready():
-	PlayerList.append("player_2")
+	if not "player_2" in PlayerList:
+		PlayerList.append("player_2")
 
 func _player3_ready():
-	PlayerList.append("player_3")
+	if not "player_3" in PlayerList:
+		PlayerList.append("player_3")
 	
 func _player4_ready():
-	PlayerList.append("player_4")
+	if not "player_4" in PlayerList:
+		PlayerList.append("player_4")
+		
+func _resetPlayerList():
+	PlayerList = ["player_1"]
