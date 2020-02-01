503c993aef939bf4fea12eb8ea32cbef4d009911
Michael Watzko <Michael.Watzko@stz-softwaretechnik.de>
2020-02-01 14:26:26 +0100

    Move interaction logic to the TilePlayer to be able to fix holes again

--- characters/players/TilePlayer.gd
+++ characters/players/TilePlayer.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const World = preload("res://levels/World.gd")
+
 var gravity = 800
 onready var velocity:Vector2 = Vector2.ZERO
 
@@ -7,6 +9,13 @@ export var speed = 65
 
 var is_on_ladder = false
 var can_jump = false
+
+export (NodePath) onready var drop_item_to
+
+# Do not export, because then it cannot be easily nulled at the beginning
+# If this is exported, there is a ghostitem in the players hand that has
+# to be discarded before he can pick up any object
+var holding
 
 func _ready():
 	pass
@@ -35,6 +44,56 @@ func _process(_delta):
 		$Animation.play("idle")
 	else:
 		$Animation.play("walk")
+
+
+	if Input.is_action_pressed("interact"):
+		var areas = $InteractableArea.get_overlapping_areas()
+		if areas.size() > 0 && areas[0].has_method("interact_with_player"):
+			print("Interacting with ", areas[0]);
+			areas[0].call("interact_with_player", self)
+			
+	if Input.is_action_pressed("drop_item"):
+		if holding != null:
+			var itemtodrop = pickupable.new()
+			itemtodrop.texture = load("res://items/interactables/bed/Bedsheet.png")
+			itemtodrop.item_type = holding
+			# Set itemposition to player position. Otherwise it will spawn
+			# at (0,0)
+			itemtodrop.position = self.position
+			get_node(drop_item_to).add_child(itemtodrop)
+		self.set_holding(null)
+		
+func can_pickup():
+	return holding == null
+		
+func take_item():
+	var item = self.holding
+	self.set_holding(null)
+	return item
+	
+func take_item_if_eq(item):
+	var holding = self.holding
+	if holding == item:
+		self.set_holding(null)
+		return holding
+	else:
+		return null
+		
+func set_holding(item):
+	holding = item
+	
+	if item == null:
+		$Holding.texture = null
+	else:
+		var asset = "";
+		match item:
+			World.Item.Gasoline:
+				asset = "gasoline/Gasoline.png"
+			World.Item.Bucket:
+				asset = "bucket/Bucket.png"
+			World.Item.Bedsheet:
+				asset = "bed/Bedsheet.png"
+		$Holding.texture = load("res://items/interactables/" + asset)
 
 
 func _physics_process(delta):
--- items/hole/Hole.gd
+++ items/hole/Hole.gd
@@ -1,11 +1,12 @@
-extends Area2D
+extends "res://items/interactable.gd"
 
 var check = 1
-const World = preload("res://levels/World.gd")
 
 func interact_with_player(player):
+	print("Player wants to interact ", player)
 	if check<2:
 		var item = player.take_item_if_eq(World.Item.Bedsheet);
 		if item != null:
-			$HoleSprite.texture = load("res://items/hole/abgedichtetesloch.jpeg")
+			$Water.texture = load("res://items/hole/abgedichtetesloch.jpeg")
+			$Animation.stop(true)
 			check = check+1
