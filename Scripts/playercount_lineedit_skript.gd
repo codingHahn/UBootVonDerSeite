8baae1fd13dd7fb16381a5a0109c66066d459cac
Daniel Schwan <daniel.schwan@it-designers.de>
2020-02-01 11:38:48 +0100

    added some ui elements

--- Scripts/playercount_lineedit_skript.gd
+++
@@ -1,5 +0,0 @@
-extends LineEdit
-
-func _on_playercount_lineedit_text_changed(new_text):
-	if(int(new_text) > 4):
-		set_text("4");
--- Scripts/start_b_script.gd
+++
@@ -1,4 +0,0 @@
-extends Button
-
-func _on_START_pressed():
-	get_tree().change_scene("res://Scenes/select_player_count.tscn")
