7cf4d77c3eae2c362decfa6efd867b17821eb063
Daniel Schwan <daniel.schwan@it-designers.de>
2020-02-01 18:32:12 +0100

    Added player with animation to player selection screen

---
+++ ui/menu/PlayerAnimation.gd
@@ -0,0 +1,4 @@
+extends KinematicBody2D
+
+func _process(delta):
+	$AnimationPlayer.play("player_1_animation")
--- ui/menu/player3_image_script.gd
+++ ui/menu/player3_image_script.gd
@@ -3,11 +3,11 @@ extends Panel
 signal Player3_ready
 
 func _ready():
-	$"player_image".hide()
+	$"Player_3/Sprite".hide()
 	connect("Player3_ready", $"/root/Global", "_player3_ready")
 	
 func _input(event):
 	if event.is_action_pressed("player_3_interact"):
-		$"player_image".show()
+		$"Player_3/Sprite".show()
 		$"Join_Player3_Text".hide()
 		emit_signal("Player3_ready")
