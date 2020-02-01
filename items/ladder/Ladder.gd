aabf50a0eda9b1cf4bf101eb0d3979e3cd63f551
Nick Hahn <nick.hahn@it-designers.de>
2020-02-01 18:21:15 +0100

    Second player unable to climb ladder

--- items/ladder/Ladder.gd
+++ items/ladder/Ladder.gd
@@ -2,11 +2,11 @@ extends Area2D
 
 
 
-func _on_Ladder_body_entered(body):
-	if body.name == "Player":
+func _on_Ladder_body_entered(body : KinematicBody2D):
+	if body.is_in_group("players"):
 		body.ladder_on = true
 
 
-func _on_Ladder_body_exited(body):
-	if body.name == "Player":
+func _on_Ladder_body_exited(body : KinematicBody2D):
+	if body.is_in_group("players"):
 		body.ladder_on = false
