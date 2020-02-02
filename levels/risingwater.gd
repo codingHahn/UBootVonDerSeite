7ac6487e1e4162560fa48b23adbb0292d5aee1bd
Julian Klissenbauer <julian.klissenbauer@stz-softwaretechnik.de>
2020-02-02 14:34:19 +0100

    Implement water based on polygon

--- levels/risingwater.gd
+++ levels/risingwater.gd
@@ -1,14 +1,23 @@
-extends Sprite
+extends Polygon2D
 
+const max_health = 1000.0
 
 var initial_scale
 var scale_fraction
 
 func _ready():
-	initial_scale = scale 
-	scale.y = scale.y / 1000
+	#initial_scale = scale 
+	#scale.y = scale.y / 1000
 	
-	scale_fraction = scale.y
+	#scale_fraction = scale.y
+	pass
 	
 func _process(_delta):
-	scale.y = scale_fraction * (1000 - $"../../Level".health)
+	var bounds = $Bounds.get_rect().size
+	
+	var percentage_destroyed = (max_health - $"../../Level".health) / max_health
+	var height = bounds.y * percentage_destroyed
+	
+	print(self.material.get_shader_param("height"), " ", bounds.y, " ", percentage_destroyed)
+	
+	self.material.set_shader_param("height", Vector3(bounds.x, height, 4096))
